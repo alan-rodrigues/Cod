1f38384bc86dfc7df79fffcfca5d8d6c46894f8e
alan-rodrigues <alan_rodrigues_florencio@hotmail.com>
2015-10-16 09:30:26 -0300

    First Program in C

---
+++ Primeiro Prog/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Primeiro Programa
+/* Programa criado por Alan Rodrigues Florencio
+ Curso de Linguagem C do portalcplusplus.com.br */
+
+int main(){
+
+    //Imprime na tela a mensagem
+    printf("Sejam bem vindos ao portal cplusplus.com.br \n ");
+    system("PAUSE");//Pausa o programa para ver o que está acontecendo
+    return 0;
+    
+}
